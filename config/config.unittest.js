87055dd5660182709580eecaa9309cff4c145f31
manager <manager@xingfufit.com>
2020-09-14 11:50:18 +0800

    初始化项目

---
+++ config/config.unittest.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// change to your own sequelize configurations for test
+exports.sequelize = {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  database: 'test',
+  username: 'root',
+  password: '',
+};
---
+++ config/plugin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
+
+exports.passport = {
+  enable: true,
+  package: 'egg-passport',
+};
